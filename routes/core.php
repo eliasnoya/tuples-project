13e1d3ee6d41bce69ad31026aaf25e227fae34e7
eliasnoya <68211494+eliasnoya@users.noreply.github.com>
2023-11-23 20:50:09 -0300

    skeleton clean 2

--- routes/core.php
+++ routes/core.php
@@ -4,8 +4,14 @@ use Src\Index\Infrastructure\IndexController;
 use Tuples\Http\Route;
 use Tuples\Http\RouteGroup;
 
-return [
 
+return [
+    // Setup your routes and/or route groups here
     Route::get("/", [IndexController::class, 'index']),
 
+    // Group example:
+    // RouteGroup::prefix("/prefix")->add([
+    //    Route::get("/", [Controller::class, 'index']),
+    //    Route::get("/{name}", [Controller::class, 'page'])
+    // ]),
 ];
